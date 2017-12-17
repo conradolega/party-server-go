c350e160d8c352a96523e8f4fc23a4cb980a898d
Conrad Olega <conradolega@gmail.com>
2017-12-17 19:35:33 +0800

    Create `Server` struct

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"net"
 )
 
-func main() {
-	fmt.Println("Starting server...")
+type Server struct {
+	clients []net.Conn
+}
+
+func (s Server) Run() {
 	listener, err := net.Listen("tcp", "0.0.0.0:3123")
 	if err != nil {
 		fmt.Println(err)
@@ -18,11 +21,18 @@ func main() {
 			fmt.Println(err)
 		}
 		fmt.Printf("Client %v connected.\n", conn.RemoteAddr())
-		go handle(conn)
+		s.clients = append(s.clients, conn)
+		go s.Handle(conn)
 	}
 }
 
-func handle(conn net.Conn) {
+func (s Server) Handle(conn net.Conn) {
 	defer conn.Close()
 	conn.Write([]byte("Hello\n"))
+}
+
+func main() {
+	fmt.Println("Starting server...")
+	server := Server{}
+	server.Run()
 }
